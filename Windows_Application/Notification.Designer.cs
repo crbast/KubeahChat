7045a80ab5b18b394e108e2df2dcf81d790f0688
CrBast <31575093+CrBast@users.noreply.github.com>
2018-04-08 21:35:36 +0200

    Add form "Notification"

---
+++ Windows_Application/Notification.Designer.cs
@@ -0,0 +1,92 @@
+﻿namespace KChat
+{
+    partial class Notification
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Notification));
+            this.tbxContent = new System.Windows.Forms.TextBox();
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.timerCloseApp = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // tbxContent
+            // 
+            this.tbxContent.BackColor = System.Drawing.SystemColors.Control;
+            this.tbxContent.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.tbxContent.Enabled = false;
+            this.tbxContent.Font = new System.Drawing.Font("Century Gothic", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbxContent.Location = new System.Drawing.Point(14, 39);
+            this.tbxContent.Multiline = true;
+            this.tbxContent.Name = "tbxContent";
+            this.tbxContent.ReadOnly = true;
+            this.tbxContent.Size = new System.Drawing.Size(271, 65);
+            this.tbxContent.TabIndex = 0;
+            this.tbxContent.Text = "Content";
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.Enabled = false;
+            this.lblTitle.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitle.Location = new System.Drawing.Point(12, 9);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(34, 16);
+            this.lblTitle.TabIndex = 1;
+            this.lblTitle.Text = "Title";
+            // 
+            // timerCloseApp
+            // 
+            this.timerCloseApp.Enabled = true;
+            this.timerCloseApp.Interval = 4000;
+            this.timerCloseApp.Tick += new System.EventHandler(this.timerCloseApp_Tick);
+            // 
+            // Notification
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(297, 110);
+            this.Controls.Add(this.lblTitle);
+            this.Controls.Add(this.tbxContent);
+            this.Cursor = System.Windows.Forms.Cursors.Default;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Notification";
+            this.Text = "Notification";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbxContent;
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.Timer timerCloseApp;
+    }
+}
---
+++ Windows_Application/Notification.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KChat
+{
+    public partial class Notification : Form
+    {
+        public Notification(string Content, string Title)
+        {
+            InitializeComponent();
+            this.Opacity = 0.8;
+            this.BackColor = Color.LightGray;
+            tbxContent.BackColor = Color.LightGray;
+            lblTitle.Text = Title;
+            tbxContent.Text = Content;
+            Rectangle workingArea = Screen.GetWorkingArea(this);
+            this.Location = new Point(workingArea.Right - Size.Width, workingArea.Bottom - Size.Height);
+        }
+
+        private void timerCloseApp_Tick(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
