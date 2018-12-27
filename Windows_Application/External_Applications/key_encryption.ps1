687dbc04a38639c13e4676256e10b8685079e0df
Bastien <bastien.crettenand@hotmail.com>
2018-12-27 14:11:31 +0100

    Add encryption class

---
+++ Windows_Application/External_Applications/key_encryption.ps1
@@ -0,0 +1,15 @@
+<#
+    Create public/private key
+    -C => Command {
+        Create -> Create public and private key
+        CreatePublic -> Create public key
+        CreatePrivate -> Create private key
+    }
+#>
+param
+(
+    [string]$C = ""
+)
+if($C = ""){
+    
+}
